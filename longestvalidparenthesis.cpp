f261b647bf1656ac9d6b179ddd0adc850bf02226
Tushar Pal <90563855+Tushar-r12345@users.noreply.github.com>
2025-04-28 15:33:12 +0530

    Update longestvalidparenthesis.cpp

--- longestvalidparenthesis.cpp
+++ longestvalidparenthesis.cpp
@@ -1,3 +1,21 @@
+/* this is the code for finding longest valid parentheses with given input and output:
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+Example 3:
+
+Input: s = ""
+Output: 0
+
+*/
+    
 #include <iostream>
 #include <string>
 #include <stack>
