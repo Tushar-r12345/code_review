61aaac4fb9becb0cb16c68bf9f22ac5052532a1b
Tushar Pal <90563855+Tushar-r12345@users.noreply.github.com>
2025-04-28 15:35:26 +0530

    Update preordertraversal.cpp

--- preordertraversal.cpp
+++ preordertraversal.cpp
@@ -1,3 +1,24 @@
+/* The question is related to preorder traversal and the input and output are as follows:
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+*/
+
+
 #include <iostream>
 #include <vector>
 
